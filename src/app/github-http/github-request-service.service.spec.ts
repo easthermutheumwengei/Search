077491c21a31ba33969eb8f2a2bf858220379f9e
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-07-26 00:59:49 +0300

    add github-http service

---
+++ src/app/github-http/github-request-service.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { GithubRequestServiceService } from './github-request-service.service';
+
+describe('GithubRequestServiceService', () => {
+  let service: GithubRequestServiceService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(GithubRequestServiceService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
