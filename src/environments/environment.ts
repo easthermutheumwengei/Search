8c29e92c13028792cd99a040ac6206c27436eae8
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-07-25 23:06:13 +0300

    create a class for users and repositoy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: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,
 };
 
 /*
