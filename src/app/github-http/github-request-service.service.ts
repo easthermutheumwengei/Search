e02f8284142bf6706337dfe1684c8657a26d1eba
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-07-26 01:11:38 +0300

    modify the export and constructor

--- src/app/github-http/github-request-service.service.ts
+++ src/app/github-http/github-request-service.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common';
+import { environment } from 'src/environments/environment.prod';
+import { User } from '../user';
+import { Repository } from '../repository';
 @Injectable({
   providedIn: 'root'
 })
 export class GithubRequestServiceService {
+  user!:User;
+  repository!:Repository;
+  repoData:any = [];
+  newUserData: any = []
+  
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.user = new User ("", 0,"","");
+    this.repository = new
+      Repository("", "", "", "")
+  }
 }
