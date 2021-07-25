634759f1ced994144a39b496a16145617633eed8
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-07-26 00:07:45 +0300

    add nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubComponent } from './github/github/github.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
